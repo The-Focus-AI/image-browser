9bae4140319ff7cbc57aa6756fffeaab6b23e4b3
Will Schenk <wschenk@gmail.com>
2025-11-05 16:50:05 -0500

    feat(browse): add favicon and static asset support
    - Add favicon.svg link to template.html for browser tab icon
    - Configure Express to serve static assets from /public directory
    - Rename mise tasks to use simpler naming convention
    - Add fast-upload task for optimized image uploading

--- browse/src/server.ts
+++ browse/src/server.ts
@@ -69,6 +69,9 @@ app.use((req, _res, next) => {
   next();
 });
 
+// Serve static assets (e.g., favicon) from /public
+app.use(express.static(path.join(__dirname, "public")));
+
 // Serve local images directory at /images
 app.use("/images", express.static(IMAGES_DIR));
 
